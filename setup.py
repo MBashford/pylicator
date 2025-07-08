b8389bf3673b57de791b63f7bbbd28be0f4dfb4f
MBashford <milobashford@gmail.com>
2025-07-08 14:54:22 +0200

    increment version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 DESCRIPTION = "A SNMP trap exploder/forwarder implemented in python"
 LONG_DESCRIPTION = """
 A SNMP trap exploder/forwarder implemented in python. Enables logging received 
